70c08419b7d8ce72771551f7171941a1a55db4bc
suzoo-dev <57764195+suzoo-dev@users.noreply.github.com>
2024-07-12 02:01:02 +0100

    working with openai

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## TODO
 
-- [ ] Link to google ai sdk with google api key.
-- [ ] Send image data along with prompt to request what the drawing is of.
+- [x] Link to AI SDK.
+- [x] Send image data along with prompt to request what the drawing is of.
 - [ ] Display the guess from the AI to the user.
-- [ ] Get the original idea for the user to draw from gemini AI.
+- [ ] Get the original idea for the user to draw from AI.
 - [ ] Display a timer for the user, if they don't get the AI to guess is it right in time they fail.
--- src/server/api/routers/guess.ts
+++ src/server/api/routers/guess.ts
@@ -1,14 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { z } from "zod";
+// import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+// const google = createGoogleGenerativeAI({
+//   apiKey: process.env.GOOGLE_API_KEY,
+// });
 
 export const guessRouter = createTRPCRouter({
   send: publicProcedure
     .input(z.object({ image: z.string() }))
     .mutation(async ({ input }) => {
       // console.log(input.image);
+      const prompt =
+        "What has been drawn in this picture? Please answer in as few words as possible.";
+
+      const { text, finishReason } = await generateText({
+        // model: google("models/gemini-1.5-pro-latest"),
+        model: openai("gpt-4-turbo"),
+        // prompt: "What is the capital of France?",
+        messages: [
+          {
+            role: "user",
+            content: [
+              {
+                type: "text",
+                text: prompt,
+              },
+              {
+                type: "image",
+                image: input.image,
+              },
+            ],
+          },
+        ],
+      });
+
       return {
-        answer: "hello",
+        answer: text,
+        finishReason,
       };
     }),
 });
